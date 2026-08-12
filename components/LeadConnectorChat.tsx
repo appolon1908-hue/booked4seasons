6597224d0104cea6e36c6ef29d69fd17e47fa1f7
Ralph Appolon <appolon1908@gmail.com>
2026-08-12 13:00:47 -0400

    feat: add responsive LeadConnector chat widget

---
+++ components/LeadConnectorChat.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+/**
+ * Booked4Seasons LeadConnector chat widget.
+ *
+ * Widget branding (avatar/header/colors) is configured inside the
+ * LeadConnector/HighLevel Chat Widget editor. Keep that editor branded to
+ * Booked4Seasons and use the approved logo asset from this repository.
+ */
+export function LeadConnectorChat() {
+  return (
+    <Script
+      id="booked4seasons-leadconnector-chat"
+      src="https://widgets.leadconnectorhq.com/loader.js"
+      data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
+      data-widget-id="6a73a20197ea74e60a1c9641"
+      data-source="WEB_USER"
+      strategy="afterInteractive"
+    />
+  );
+}
